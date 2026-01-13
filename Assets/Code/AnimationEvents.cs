3420c4713f5de0cc0b04c3ebbe2316954b67952f
Jackenobi <jacksilar@web.de>
2026-01-13 23:02:53 +0100

    footsteps gehen wieder aber noch keine unterschiedlichen

--- Assets/Code/AnimationEvents.cs
+++ Assets/Code/AnimationEvents.cs
@@ -2,35 +2,33 @@ using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
 
-public class AnimationEvents : MonoBehaviour 
+public class AnimationEvents : MonoBehaviour
 {
-
     private EventInstance footstepInstance;
-
     public EventReference footstepReference;
     public LayerMask groundMaterialLayer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //wir sagen FMOD, dass wir sp‰ter dieses Event abspielen wollen
+        // Wir sagen FMOD, dass wir sp‰ter dieses Event abspielen wollen
         footstepInstance = RuntimeManager.CreateInstance(footstepReference);
     }
 
     public void Footstep()
     {
-        if(Physics.Raycast(transform.position + new Vector3(0, 2, 0), Vector3.down, out RaycastHit hitinfo, 2, groundMaterialLayer))
+        if (Physics.Raycast(transform.position + new Vector3(0, 2, 0), Vector3.down, out RaycastHit hitinfo, 2, groundMaterialLayer))
         {
-            footstepInstance.setParameterByNameWithLabel("NameVomParameterinFMOD", hitinfo.transform.tag);
+            // Parameter-Name aus FMOD: "Footstep_ground"
+            // Wert: Tag vom GameObject (z.B. "Straﬂe", "Forest", "Sand")
+            footstepInstance.setParameterByNameWithLabel("Footstep_ground", hitinfo.transform.tag);
         }
         else
         {
-            footstepInstance.setParameterByNameWithLabel("NameVomParameterinFMOD", "BasicFootstepswennnichtsanderesdaist");
+            // Fallback wenn kein Boden getroffen wird
+            footstepInstance.setParameterByNameWithLabel("Footstep_ground", "Straﬂe");
         }
 
-
-            footstepInstance.set3DAttributes(RuntimeUtils.To3DAttributes(transform));
+        footstepInstance.set3DAttributes(RuntimeUtils.To3DAttributes(transform));
         footstepInstance.start();
     }
-
 }
